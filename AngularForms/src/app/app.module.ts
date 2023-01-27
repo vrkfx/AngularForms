2f600afd1913c48170c559aa9a8aeed7df4f9dfc
Virroshan_Kumar <virroshan08@outlook.com>
2023-01-27 23:25:27 +1030

    Learning Simple FormBuilder Form Group and Form Related Stuff

--- AngularForms/src/app/app.module.ts
+++ AngularForms/src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field'
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatSuffix} from '@angular/material/form-field';
-
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
--- AngularForms/src/app/form/form.component.ts
+++ AngularForms/src/app/form/form.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent implements OnInit {
-  public BookName: any = "";
-  public Author: any = "";
+export class FormComponent {
+  // public BookName: any = "";
+  // public Author: any = "";
+
+  form: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+      this.form = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+      });
+  }
 
 
-  // constructor() {
-  //   this.value = "Two way Biniding"
-  // }
-
+  //ON Initialised
   ngOnInit(): void {
 
   }
@@ -21,5 +31,9 @@ export class FormComponent implements OnInit {
   public button(){
     console.log("Button Clicked")
   }
+
+  onFormSubmit() {
+    console.log(this.form.value);
+}
 
 }
